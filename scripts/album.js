7a22032c6235030927b9119a058396636a3aa3de
rghuser <rghuser@gmail.com>
2017-01-15 08:35:12 -0500

    Assignment-20-jQuery-Buzz-Library: Completed

--- scripts/album.js
+++ scripts/album.js
@@ -190,6 +190,19 @@ var previousSong = function() {
     $lastSongNumberCell.html(lastSongNumber);    
 };
 
+var togglePlayFromPlayerbar = function() {
+     var $currentlyPlayingCell = getSongNumberCell(currentlyPlayingSongNumber);
+     if (currentSoundFile.isPaused()) {
+         $currentlyPlayingCell.html(pauseButtonTemplate);
+         $(this).html(playerBarPauseButton);
+         currentSoundFile.play();
+     } else if (currentSoundFile) {
+         $currentlyPlayingCell.html(playButtonTemplate);
+         $(this).html(playerBarPlayButton);
+         currentSoundFile.pause();
+     }
+ };
+
  // Album button templates
  var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
  var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
@@ -207,11 +220,13 @@ var previousSong = function() {
 
  var $previousButton = $('.main-controls .previous');
  var $nextButton = $('.main-controls .next');
+ var $playPauseButton = $('.main-controls .play-pause');
       
  $(document).ready(function() {
       setCurrentAlbum(albumPicasso); 
 	  $previousButton.click(previousSong);
       $nextButton.click(nextSong);
+	  $playPauseButton.click(togglePlayFromPlayerbar);
  });
 
 // // Another Example Album 3
