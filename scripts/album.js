954fd177d89bed3667c804c1ca0ec4ebf53d43c8
rghuser <rghuser@gmail.com>
2017-01-15 07:59:50 -0500

    Checkpoint-20-jQuery-Buzz-Library: Completed

--- scripts/album.js
+++ scripts/album.js
@@ -25,15 +25,20 @@ var createSongRow = function(songNumber, songName, songLength) {
 		    // Switch from Play -> Pause button to indicate new song is playing.
 		    $(this).html(pauseButtonTemplate);
 		    setSong(songNumber);
+			currentSoundFile.play();
 			currentSongFromAlbum = currentAlbum.songs[songNumber - 1];
 			updatePlayerBarSong();
 	    } else if (currentlyPlayingSongNumber === songNumber) {
-		   // Switch from Pause -> Play button to pause currently playing song.
-		   $(this).html(playButtonTemplate);
-		   $('.main-controls .play-pause').html(playerBarPlayButton);
-		   currentlyPlayingSongNumber = null;
-		   currentSongFromAlbum = null;
-	    }
+		     if (currentSoundFile.isPaused()) {
+                 $(this).html(pauseButtonTemplate);
+                 $('.main-controls .play-pause').html(playerBarPauseButton);
+                 currentSoundFile.play();
+             } else {
+                 $(this).html(playButtonTemplate);
+                 $('.main-controls .play-pause').html(playerBarPlayButton);
+                 currentSoundFile.pause();   
+             }
+	   }
 };
  
     var onHover = function(event) {
@@ -63,8 +68,25 @@ var createSongRow = function(songNumber, songName, songLength) {
  };
 
 var setSong = function(songNumber) {
+	if (currentSoundFile) {
+         currentSoundFile.stop();
+     }
+	
     currentlyPlayingSongNumber = parseInt(songNumber);
-    currentSongFromAlbum = currentAlbum.songs[songNumber - 1];    
+    currentSongFromAlbum = currentAlbum.songs[songNumber - 1];
+	// #1
+	currentSoundFile = new buzz.sound(currentSongFromAlbum.audioUrl, {
+         // #2
+         formats: [ 'mp3' ],
+         preload: true
+     });
+	setVolume(currentVolume);
+ };
+
+ var setVolume = function(volume) {
+     if (currentSoundFile) {
+         currentSoundFile.setVolume(volume);
+     }
  };
 
 var setCurrentAlbum = function(album) { 
@@ -118,6 +140,7 @@ var nextSong = function() {
     
     // Set a new current song
     setSong(currentSongIndex + 1);
+	currentSoundFile.play();
     currentSongFromAlbum = currentAlbum.songs[currentSongIndex];
 
     // Update the Player Bar information
@@ -150,6 +173,7 @@ var previousSong = function() {
     
     // Set a new current song
     setSong(currentSongIndex + 1);
+	currentSoundFile.play();
     currentSongFromAlbum = currentAlbum.songs[currentSongIndex];
 
     // Update the Player Bar information
@@ -178,6 +202,8 @@ var previousSong = function() {
  var currentAlbum = null;
  var currentlyPlayingSongNumber = null;
  var currentSongFromAlbum = null;
+ var currentSoundFile = null;
+ var currentVolume = 80;
 
  var $previousButton = $('.main-controls .previous');
  var $nextButton = $('.main-controls .next');
